9b8a9491c27e880272e25005183e45ec39caf50d
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2024-01-19 14:40:00 +0100

    Cards fertig

--- demoproject2/src/components/Cardwrapper.js
+++ demoproject2/src/components/Cardwrapper.js
@@ -11,47 +11,47 @@ export default function Cardwrapper(params) {
       <Card
         name="jonas kronabitter"
         title="Codierer"
-        imglink="https://picsum.photos/100/150"
+        imglink="https://picsum.photos/101/150"
       ></Card>
       <Card
         name="jonas kronabitter"
         title="Codierer"
-        imglink="https://picsum.photos/100/150"
+        imglink="https://picsum.photos/102/150"
       ></Card>
       <Card
         name="jonas kronabitter"
         title="Codierer"
-        imglink="https://picsum.photos/100/150"
+        imglink="https://picsum.photos/103/150"
       ></Card>
       <Card
         name="jonas kronabitter"
         title="Codierer"
-        imglink="https://picsum.photos/100/150"
+        imglink="https://picsum.photos/104/150"
       ></Card>
       <Card
         name="jonas kronabitter"
         title="Codierer"
-        imglink="https://picsum.photos/100/150"
+        imglink="https://picsum.photos/105/150"
       ></Card>
       <Card
         name="jonas kronabitter"
         title="Codierer"
-        imglink="https://picsum.photos/100/150"
+        imglink="https://picsum.photos/100/151"
       ></Card>
       <Card
         name="jonas kronabitter"
         title="Codierer"
-        imglink="https://picsum.photos/100/150"
+        imglink="https://picsum.photos/100/152"
       ></Card>
       <Card
         name="jonas kronabitter"
         title="Codierer"
-        imglink="https://picsum.photos/100/150"
+        imglink="https://picsum.photos/100/153"
       ></Card>
       <Card
         name="jonas kronabitter"
         title="Codierer"
-        imglink="https://picsum.photos/100/150"
+        imglink="https://picsum.photos/100/154"
       ></Card>
     </div>
   );
