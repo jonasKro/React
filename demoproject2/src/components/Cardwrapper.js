00404f20f3ff8fec883a3a58a3ac775f45f4b277
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2024-02-28 10:12:46 +0100

    Filter fertig

--- demoproject2/src/components/Cardwrapper.js
+++ demoproject2/src/components/Cardwrapper.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 
 export default function Cardwrapper(params) {
   const [People, setPeople] = useState([]);
+  const [filteredpeople, setFilteredPeople] = useState([]);
 
   useEffect(() => {
     fetch("https://65b0e037d16d31d11bdd7634.mockapi.io/cardsAPI/person").then(
@@ -10,21 +11,38 @@ export default function Cardwrapper(params) {
         res.json().then((data) => {
           console.log(data);
           setPeople(data);
+          setFilteredPeople(data);
         })
     );
   }, []);
 
+  const filterPeople = (filter) => {
+    let filtered = People.filter((person) => person.name.includes(filter));
+    setFilteredPeople(filtered);
+  };
+
   return (
-    <div class="Wrapper">
-      {People.map((element) => {
-        return (
-          <Card
-            name={element.name}
-            title={element.jobtitle}
-            imglink={element.avatar}
-          ></Card>
-        );
-      })}
+    <div>
+      <input
+        className="Input"
+        placeholder="search"
+        type="text"
+        onChange={(el) => {
+          console.log(el.target.value);
+          filterPeople(el.target.value);
+        }}
+      ></input>
+      <div class="Wrapper">
+        {filteredpeople.map((element) => {
+          return (
+            <Card
+              name={element.name}
+              title={element.jobtitle}
+              imglink={element.avatar}
+            ></Card>
+          );
+        })}
+      </div>
     </div>
   );
 }
